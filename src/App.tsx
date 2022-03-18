f6b93651b2ccf17685a7e112e9fcd1a058f4047f
KaranThakkarPrescient <karan.thakkar@prescient.co.in>
2022-03-18 19:37:09 +0530

    scroll effect done perfectly

--- src/App.tsx
+++ src/App.tsx
@@ -61,8 +61,33 @@ function App() {
             src="https://nft.tinyfac.es/static/media/character15.ab1f4b32469e93d54104.jpg"
             alt=""
           />
+          {/* extra images below for scrolling effect to not break */}
           <img
             src="https://nft.tinyfac.es/static/media/character1.757314c824d50099d4f4.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character5.bbee78a943e74e125ace.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character6.887b065876892a3bc33b.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character7.5dc1143d6e41e775d01f.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character8.a09f097065b0483eec56.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character15.ab1f4b32469e93d54104.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character13.67344d9c9b864a39b6cb.jpg"
             alt=""
           />
         </div>
