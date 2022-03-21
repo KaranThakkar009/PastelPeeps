b2efbbce4a1b3da650c7cacc779bd3cb422eff55
KaranThakkarPrescient <karan.thakkar@prescient.co.in>
2022-03-21 19:59:20 +0530

    navbar completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,58 @@
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const handleClick = () => {
+    const checkbox = document.getElementById("check") as HTMLInputElement;
+    checkbox.checked = false;
+  };
+
   return (
     <>
-      {/* <header className="header">
-        <div className="logo">PastelPeeps</div>
-        <nav>
-          <ul className="menu-options">
-            <li>Menu</li>
-            <li>Prelude</li>
-            <li>Metaverse Mall</li>
-            <li>FAQs</li>
-            <li>Twitter</li>
-            <li>Discord</li>
-            <li>Instagram</li>
-          </ul>
-        </nav>
-        <button className="connect-btn">Connect</button>
-      </header> */}
+      <nav>
+        <input type="checkbox" name="checkbox" id="check" />
+        <label htmlFor="check">
+          <i className="fas fa-bars" id="btn"></i>
+          <i className="fas fa-times" id="cancel"></i>
+        </label>
+        <img src="https://doodles.app/images/doodles_logo.png" alt="" />
+        <ul>
+          <li>
+            <a onClick={() => handleClick()} href="#">
+              Prelude
+            </a>
+          </li>
+          <li>
+            <a onClick={() => handleClick()} href="#">
+              Metaverse Mall
+            </a>
+          </li>
+          <li className="menu-items">
+            <a onClick={() => handleClick()} href="#">
+              FAQs
+            </a>
+          </li>
+          <li>
+            <button className="connect-btn">Connect</button>
+          </li>
+        </ul>
+      </nav>
 
       <div className="marquee">
         <div className="marquee-content">
           <img
-            src="https://nft.tinyfac.es/static/media/character1.757314c824d50099d4f4.jpg"
+            src="https://nft.tinyfac.es/static/media/character8.046243586899b36b2090.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character5.bbee78a943e74e125ace.jpg"
+            src="https://nft.tinyfac.es/static/media/character1.e0a210e92ec4cbed8a2a.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character6.887b065876892a3bc33b.jpg"
+            src="https://nft.tinyfac.es/static/media/character2.403babe869f3adee74b5.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character7.5dc1143d6e41e775d01f.jpg"
-            alt=""
-          />
-          <img
-            src="https://nft.tinyfac.es/static/media/character8.a09f097065b0483eec56.jpg"
-            alt=""
-          />
-          <img
-            src="https://nft.tinyfac.es/static/media/character15.ab1f4b32469e93d54104.jpg"
-            alt=""
-          />
-          <img
-            src="https://nft.tinyfac.es/static/media/character13.67344d9c9b864a39b6cb.jpg"
-            alt=""
-          />
-          <img
-            src="https://nft.tinyfac.es/static/media/character10.6ccb548704fd7fd550b8.jpg"
-            alt=""
-          />
-          <img
-            src="https://nft.tinyfac.es/static/media/character14.2f8d84acfeb4457336fa.jpg"
-            alt=""
-          />
-          <img
-            src="https://nft.tinyfac.es/static/media/character9.fd1bed3c1f61bae6b000.jpg"
+            src="https://nft.tinyfac.es/static/media/character3.5902788d18338970e54c.jpg"
             alt=""
           />
           <img
@@ -67,50 +60,65 @@ function App() {
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character16.45b779caa2c5cd49533c.jpg"
+            src="https://nft.tinyfac.es/static/media/character8.046243586899b36b2090.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character6.887b065876892a3bc33b.jpg"
+            src="https://nft.tinyfac.es/static/media/character1.e0a210e92ec4cbed8a2a.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character15.ab1f4b32469e93d54104.jpg"
-            alt=""
-          />
-          {/* extra images below for scrolling effect to not break */}
-          <img
-            src="https://nft.tinyfac.es/static/media/character1.757314c824d50099d4f4.jpg"
+            src="https://nft.tinyfac.es/static/media/character2.403babe869f3adee74b5.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character5.bbee78a943e74e125ace.jpg"
+            src="https://nft.tinyfac.es/static/media/character3.5902788d18338970e54c.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character6.887b065876892a3bc33b.jpg"
+            src="https://nft.tinyfac.es/static/media/character4.d8a559ffdc66efd715a4.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character7.5dc1143d6e41e775d01f.jpg"
+            src="https://nft.tinyfac.es/static/media/character8.046243586899b36b2090.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character8.a09f097065b0483eec56.jpg"
-            alt=""
-          />
-          {/* <img
-            src="https://nft.tinyfac.es/static/media/character15.ab1f4b32469e93d54104.jpg"
+            src="https://nft.tinyfac.es/static/media/character1.e0a210e92ec4cbed8a2a.jpg"
             alt=""
           />
           <img
-            src="https://nft.tinyfac.es/static/media/character13.67344d9c9b864a39b6cb.jpg"
+            src="https://nft.tinyfac.es/static/media/character2.403babe869f3adee74b5.jpg"
             alt=""
-          /> */}
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character3.5902788d18338970e54c.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character8.046243586899b36b2090.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character1.e0a210e92ec4cbed8a2a.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character2.403babe869f3adee74b5.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character3.5902788d18338970e54c.jpg"
+            alt=""
+          />
+          <img
+            src="https://nft.tinyfac.es/static/media/character4.d8a559ffdc66efd715a4.jpg"
+            alt=""
+          />
         </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
